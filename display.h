f80dc641d585225449d10b5eaf62072fb5a1724a
Senithu Piumjith <fernandopsenithu@gmail.com>
2026-07-19 13:49:41 +0530

    Update display.h

--- display.h
+++ display.h
@@ -3,7 +3,9 @@
 
 #include "movie.h"
 
-void printMainMenu(void);
+void printRoleMenu(void);
+void printCustomerMenu(void);
+void printAdminMenu(void);
 void printShowtimesList(Movie movies[NUM_MOVIES]);
 void printSeatMap(Showtime *show);
 void printRevenueReport(Movie movies[NUM_MOVIES]);
